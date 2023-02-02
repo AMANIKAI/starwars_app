b1b0c45c8d841d39319809ffe5aba1a8be5a96a4
amani <akai@safaricom.co.ke>
2023-02-02 22:24:07 +0300

    updated app

--- server.js
+++ server.js
@@ -1,20 +1,63 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
 const app = express();
 
 // Make sure you place body-parser before your CRUD handlers!
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.listen(3000,function() {
+app.listen(3000, function () {
     console.log("listening on 3000")
 })
 
-// All your handlers here
-app.get('/', (req, res)=>{
+let DB_NAME = process.env.DB_NAME;
+let DB_USER = process.env.DB_USER;
+let DB_PASS = process.env.DB_PASS;
+let DB_URL = process.env.DB_URL
+let db_url = "mongodb+srv://" + DB_USER + ":" + DB_PASS + "@starwarsapp.nbk2k5v.mongodb.net/" + DB_NAME;
+console.log(db_url);
+let option = {
+    maxPoolSize: 10,
+    family: 4
+};
+mongoose.connect(db_url, option);
+let db = mongoose.connection
+db.once("open", () => {
+    console.log("Successful connection to the database");
+});
+
+db.on("error", () => {
+    console.log("Connection to DB failed");
+});
+
+const userSchema = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    secondName: { type: String, required: true },
+    emailAddress: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    timmestamp: { type: Date, default: Date.now, immutable: true }
+
+});
+
+let User = mongoose.model('User', userSchema);
+let user;
+
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
-    console.log('Hellooo!')
+})
+app.post('/quotes', (req, res) => {
+    user = {
+        firstName: req.body.firstName,
+        secondName: req.body.secondName,
+        emailAddress: req.body.email,
+        password: req.body.pass,
+    }
+
+    let newUser = new User(user);
+    newUser.save();
+    res.redirect('/');
 })
 
-app.post('/quotes', (req, res) => {
-    console.log('Hellooo!')
-})
+
+
+
